a622aadbe496d7f41289507f55617d56dbdc2e73
Nikita Dmitriev <dmitnikita@ya.ru>
2020-07-08 11:52:29 +0300

    third commit

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
-const ip = require('ip');
-const chalk = require('chalk');
 const express = require('express');
 const next = require('next').default;
 const helmet = require('helmet');
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -15,23 +12,8 @@ app.prepare().then(() => {
 
   server.use(helmet());
 
-  ['/api', '/data', '/tensorboard/proxy'].map((url) =>
-    server.use(
-      url,
-      createProxyMiddleware({
-        target: process.env.API_URL,
-        changeOrigin: true,
-        secure: false,
-      })
-    )
-  );
-
   server.get('*', (req, res) => handle(req, res));
 
   const port = parseInt(process.env.PORT, 10) || 3000;
-  server.listen(port, (err) => {
-    if (err) {
-      throw err;
-    }
-  });
+  server.listen(port);
 });
