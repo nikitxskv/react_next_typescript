01458e28a97bbe9ecabacc9742a0c5ba12a3fcfc
Nikita Dmitriev <dmitnikita@ya.ru>
2020-07-08 11:46:52 +0300

    second commit

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ const next = require('next').default;
 const helmet = require('helmet');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const logger = require('./shared/helpers/logger');
-
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 
@@ -24,28 +22,16 @@ app.prepare().then(() => {
         target: process.env.API_URL,
         changeOrigin: true,
         secure: false,
-      }),
-    ),
+      })
+    )
   );
 
   server.get('*', (req, res) => handle(req, res));
 
   const port = parseInt(process.env.PORT, 10) || 3000;
   server.listen(port, (err) => {
-    if (err) throw err;
-
-    if (dev) {
-      logger.log('info', chalk.magenta('Server is running'));
-      logger.log('info', chalk.magenta(`> Local on http://localhost:${port}`));
-      logger.log(
-        'info',
-        chalk.magenta(`> Lan on http://${ip.address()}:${port}`),
-      );
-      logger.log('info', chalk.magenta(`> API_URL is ${process.env.API_URL}`));
-    } else {
-      logger.log('info', 'Server is running');
-      logger.log('info', `> Lan on http://${ip.address()}:${port}`);
-      logger.log('info', `> API_URL is ${process.env.API_URL}`);
+    if (err) {
+      throw err;
     }
   });
 });
